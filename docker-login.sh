a6480863fe03e7c9abf89fd1f84a258cebfc0964
amans211 <amans@bitvividsolutions.com>
2025-01-11 18:34:18 +0530

    new installer changes

--- docker-login.sh
+++ docker-login.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-echo "dckr_pat_erttrtrty909065576___" | docker login -u xconnected --password-stdin
+echo "your_pat" | docker login -u xconnected --password-stdin
 
 kubectl create secret generic regcred \
     --from-file=.dockerconfigjson=/home/$USER/.docker/config.json \
